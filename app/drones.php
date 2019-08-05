3185f0669fbcc6dc46046ade0099bb03a56b47b2
danielriochristian <daniel41697@gmail.com>
2019-08-05 17:58:32 +0700

    fix approval

--- app/drones.php
+++ app/drones.php
@@ -3,11 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class drones extends Model
 {
-    use SoftDeletes;
-    protected $dates =['deleted_at'];
     protected $table = 'drones';
 }
